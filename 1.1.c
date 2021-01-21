f9b3db6e8a42cf4b449df62300ba6f4deba02df6
Ayush kumar <67006255+Ayush7614@users.noreply.github.com>
2021-01-21 10:51:20 +0530

    Update 1.1.c

--- 1.1.c
+++ 1.1.c
@@ -6,4 +6,9 @@ printf("Enter a float number : ");
 scanf("%f",&a);
 printf("Float number with two decimal points is  =%.2f",a);
 }
-
+/*
+Enter a float number : 37.666
+Float number with two decimal points is  =37.67
+--------------------------------
+Process exited after 5.493 seconds with return value 47
+Press any key to continue . . .*/
