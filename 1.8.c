aaaa208dd325f99d4a2c91153a69d04fd48d5827
Ayush kumar <67006255+Ayush7614@users.noreply.github.com>
2021-01-21 11:24:30 +0530

    Create 1.8.c

---
+++ 1.8.c
@@ -0,0 +1,15 @@
+/*1.8. Write a program to input a number and print corresponding character*/
+#include<stdio.h>
+int main(){
+  int a;
+  printf("Enter a number : ");
+  scanf("%d",&a);
+  printf("Character corresponding to %d is =%c",a,a);
+}
+/*
+Enter a number : 97
+Character corresponding to 97 is =a
+--------------------------------
+Process exited after 5.897 seconds with return value 35
+Press any key to continue . . .
+*/
