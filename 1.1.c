9a400e8e6c4b5209f3ca2109a6ad29f5b1795ce9
Ayush kumar <67006255+Ayush7614@users.noreply.github.com>
2021-01-21 10:50:44 +0530

    Add files via upload

---
+++ 1.1.c
@@ -0,0 +1,9 @@
+/*1.1. Write a program to input a floating number and show that number with two decimal points*/
+#include<stdio.h>
+int main(){
+float a;
+printf("Enter a float number : ");
+scanf("%f",&a);
+printf("Float number with two decimal points is  =%.2f",a);
+}
+
