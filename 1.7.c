c3d64a9a166cf13cad134a931f7f89d72e093358
Ayush kumar <67006255+Ayush7614@users.noreply.github.com>
2021-01-21 11:18:18 +0530

    Create 1.7.c

---
+++ 1.7.c
@@ -0,0 +1,17 @@
+/*1.7. Write a program to input a character and its ASCII value*/
+#include <stdio.h>  
+int main()  
+{  
+    char a; 
+    printf("Enter a character");  
+    scanf("%c",&a); 
+    printf("\n The ascii value of the a variable is : %d", a);  
+    return 0;  
+}  
+/*  Enter a characterb
+
+ The ascii value of the a variable is : 98
+--------------------------------
+Process exited after 2.007 seconds with return value 0
+Press any key to continue . . .
+*/
