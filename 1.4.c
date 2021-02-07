81e55fe88c73aaf26d0e7b5e866f30b7c5d86312
Ayush kumar <67006255+Ayush7614@users.noreply.github.com>
2021-02-07 19:36:34 +0530

    Create 1.4.c

---
+++ 1.4.c
@@ -0,0 +1,15 @@
+/*1.4. Write a program input a decimal number and print into decimal, octal and hexa decimal*/
+#include<stdio.h>
+int main(){
+int a;
+printf("Enter a decimal number : ");
+scanf("%d",&a);
+printf("%d in octal=%o and in hexa decimal=%x",a,a,a);
+
+}
+/* Enter a decimal number : 76
+76 in octal=114 and in hexa decimal=4c
+--------------------------------
+Process exited after 2.716 seconds with return value 38
+Press any key to continue . . .
+*/
